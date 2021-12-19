c498fc643a6f1d30015d8567fec8d482817cd187
xio810 <boi4806@naver.com>
2021-12-19 16:20:13 +0900

    서블릿 크롬으로 실행

---
+++ src/main/java/com/xio/jsp/ex/UsrHomeMain.java
@@ -0,0 +1,31 @@
+package com.xio.jsp.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/usr/home/main")
+public class UsrHomeMain extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public UsrHomeMain() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
