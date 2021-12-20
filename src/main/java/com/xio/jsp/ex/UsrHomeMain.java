78129fb355e6b6a3dc779e51c76b2c58172ccaac
xio810 <boi4806@naver.com>
2021-12-20 20:22:51 +0900

    서블릿 UTF-8 설정

--- src/main/java/com/xio/jsp/ex/UsrHomeMain.java
+++ src/main/java/com/xio/jsp/ex/UsrHomeMain.java
@@ -9,17 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/usr/home/main")
 public class UsrHomeMain extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	public UsrHomeMain() {
-		super();
-
-	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String name = request.getParameter("name");
 
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 들어오는 파리미터를 UTF-8로 해석
+		request.setCharacterEncoding("UTF-8");
+		
+		//서블릿이 HTML 파일을 만들때 UTF-8 로 쓰기
+		response.setCharacterEncoding("UTF-8");
+		// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+		response.setContentType("text/html; charset=UTF-8");
+		
+		response.getWriter().append(name);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
