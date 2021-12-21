1188c6d69396868ac0a80a0f7de13cb7cbcecde0
xio810 <boi4806@naver.com>
2021-12-21 12:22:01 +0900

    게시물 등록 서블릿

--- src/main/java/com/xio/jsp/ex/servlet/UsrArticleWriteServlet.java
+++ src/main/java/com/xio/jsp/ex/servlet/UsrArticleWriteServlet.java
@@ -1,27 +1,30 @@
-package com.xio.jsp.ex;
+package com.xio.jsp.ex.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/usr/home/main")
-public class UsrHomeMain extends HttpServlet {
+@WebServlet("/usr/article/write")
+public class UsrArticleWriteServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String name = request.getParameter("name");
 
 		// 들어오는 파리미터를 UTF-8로 해석
 		request.setCharacterEncoding("UTF-8");
-		
-		//서블릿이 HTML 파일을 만들때 UTF-8 로 쓰기
+
+		// 서블릿이 HTML 파일을 만들때 UTF-8 로 쓰기
 		response.setCharacterEncoding("UTF-8");
 		// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
 		response.setContentType("text/html; charset=UTF-8");
-		
-		response.getWriter().append(name);
+
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/usr/article/write.jsp");
+		requestDispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
---
+++ src/main/java/com/xio/jsp/ex/servlet/UsrHomeMainServlet.java
@@ -0,0 +1,32 @@
+package com.xio.jsp.ex.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/usr/home/main")
+public class UsrHomeMainServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("name");
+
+		// 들어오는 파리미터를 UTF-8로 해석
+		request.setCharacterEncoding("UTF-8");
+		
+		//서블릿이 HTML 파일을 만들때 UTF-8 로 쓰기
+		response.setCharacterEncoding("UTF-8");
+		// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+		response.setContentType("text/html; charset=UTF-8");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
