4ec8622cc4f9d69b0e026fc57126477fc52180e7
xio810 <boi4806@naver.com>
2022-01-05 02:55:03 +0900

    db.sql 추가

---
+++ db.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS jsp_board;
+CREATE DATABASE jsp_board;
+USE jsp_board;
+
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
