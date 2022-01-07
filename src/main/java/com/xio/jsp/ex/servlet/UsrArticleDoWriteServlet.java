937301e78de684563db1696d854a0b732ace85a4
xio810 <boi4806@naver.com>
2022-01-07 14:53:55 +0900

    게시물 작성 처리 mysql 도입

--- src/main/java/com/xio/jsp/ex/servlet/UsrArticleDoWriteServlet.java
+++ src/main/java/com/xio/jsp/ex/servlet/UsrArticleDoWriteServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.xio.mysqlutil.MysqlUtil;
+import com.xio.mysqlutil.SecSql;
+
 @WebServlet("/usr/article/doWrite")
 public class UsrArticleDoWriteServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -20,9 +23,25 @@ public class UsrArticleDoWriteServlet extends HttpServlet {
 		// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
 		response.setContentType("text/html; charset=UTF-8");
 
+		MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "jsp_board");
+		MysqlUtil.setDevMode(true);
+
 		String title = request.getParameter("title");
 		String body = request.getParameter("body");
-		
+
+		SecSql sql = new SecSql();
+		sql.append("INSERT INTO article");
+		sql.append("SET regDate = NOW()");
+		sql.append(", updateDate = NOW()");
+		sql.append(", title = ?", title);
+		sql.append(", `body` = ?", body);
+		int id = MysqlUtil.insert(sql);
+
+		response.getWriter().append(id + "번 게시물이 생성되었습니다.");
+
+		MysqlUtil.closeConnection();
+
+		MysqlUtil.closeConnection();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
